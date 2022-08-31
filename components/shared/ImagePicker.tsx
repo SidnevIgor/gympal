d8392584cf0707edb80ce18920a5901dab405087
SidnevIgor <mr.igor.sidnev@gmail.com>
2022-08-31 18:52:18 +0300

    Removed unused code

--- components/shared/ImagePicker.tsx
+++ components/shared/ImagePicker.tsx
@@ -1,34 +1,27 @@
 import React from 'react';
-import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
-import { launchImageLibrary } from 'react-native-image-picker';
+import {View, TouchableWithoutFeedback} from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
 
-const ImagePicker = ({ children, onImagePick }) => {
-
+const ImagePicker = ({children, onImagePick}) => {
   const openImagePicker = async () => {
     try {
       const img = await launchImageLibrary({
-        mediaType: "photo"
+        mediaType: 'photo',
       });
-      const uri = img.assets && img.assets[0].uri ? img.assets[0].uri: '';
+      const uri = img.assets && img.assets[0].uri ? img.assets[0].uri : '';
       onImagePick(uri);
     } catch (err) {
-      console.log("Could not retrieve file - ", err);
+      console.log('Could not retrieve file - ', err);
     }
-  }
+  };
 
   return (
-    <View style={styles.imagePickerWrapper}>
-      <TouchableWithoutFeedback onPress={ () => openImagePicker()}>
+    <View>
+      <TouchableWithoutFeedback onPress={() => openImagePicker()}>
         {children}
       </TouchableWithoutFeedback>
     </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  imagePickerWrapper: {
-    
-  }
-})
+  );
+};
 
 export default ImagePicker;
