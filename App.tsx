a56a990562eee1a7eb05fb20dab3e553f58837fd
SidnevIgor <mr.igor.sidnev@gmail.com>
2022-09-01 16:19:58 +0300

    Fixing assets and adding more routes

--- App.tsx
+++ App.tsx
@@ -3,14 +3,18 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import WelcomeScreen from './components/WelcomeScreen';
+import LoginScreen from './components/forms/Login/LoginScreen';
+import RegisterScreen from './components/forms/Register/RegisterScreen';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
